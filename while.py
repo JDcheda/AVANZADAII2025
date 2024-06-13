03dc8d8d2f70a6771dfb382f018e493eeef6f009
JDcheda <jdmejia1@uth.hn>
2024-06-13 16:35:40 -0600

    Update

---
+++ while.py
@@ -0,0 +1,20 @@
+import os
+os.system('cls' if os.name == 'nt' else 'clear')
+
+# i=0
+# while i<10:
+#     print(i)
+#     i+=1
+
+# print("Ciclo controlado por banderin")
+
+# while True:
+#     entrada = input()
+#     if entrada.upper == 'S':
+#         break
+#     print("Escribiste: ", entrada)
+
+print("Ciclo controlado por banderin 2")
+bandera = False
+while bandera != True:
+    bandera = input("Ingrese S para terminar: ")
